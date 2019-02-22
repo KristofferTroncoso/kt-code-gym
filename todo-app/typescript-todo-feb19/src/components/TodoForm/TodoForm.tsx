7cb91bd7aea9cfa5c6ce2099bd9928736e460fa3
Kristoffer <troncosokristoffer@gmail.com>
2019-02-22 02:22:55 -0500

    Complete TodoForm

--- todo-app/typescript-todo-feb19/src/components/TodoForm/TodoForm.tsx
+++ todo-app/typescript-todo-feb19/src/components/TodoForm/TodoForm.tsx
@@ -4,23 +4,32 @@ interface Props {
   addTodo: Function
 }
 
-class TodoForm extends Component<Props, {}> {
+interface State {
+  text: string
+}
+
+class TodoForm extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      text: 'yo'
+      text: ''
     }
   }
 
   addTodo = (e: any) => {
     e.preventDefault();
-    this.props.addTodo(e.target.value);
+    this.props.addTodo(this.state.text);
+    this.setState({text: ''});
   }
   
   render() {
     return (
       <form  onSubmit={this.addTodo}>
-        <input type="text"></input>
+        <input 
+          type="text" 
+          value={this.state.text} 
+          onChange={e => this.setState({text: e.target.value})}
+        />
       </form>
     )
   }
